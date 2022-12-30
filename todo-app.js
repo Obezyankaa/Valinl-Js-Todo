d004bcd39f5a8faffd4f2a7d77ea60aa137b7be7
VladAir <vladair@MacBook-Air-VladAir.local>
2022-12-30 17:15:04 +0300

    fix 1.6

--- todo-app.js
+++ todo-app.js
@@ -229,18 +229,17 @@
         if (!("Notification" in window))
             alert('Ваш браузер не поддерживает уведомления.');
         else if (Notification.permission === 'granted')
-            setTimeout(notifyMe, 2000);
+            setTimeout(notifyMe(), 2000);
         else if (Notification.permission !== "denied") {
             Notification.requestPermission(function (permission) {
                 if (!('permission' in Notification))
                     Notification.permission == permission;
                 if (permission === 'granted')
-                    setTimeout(notifyMe, 2000);
+                    setTimeout(notifyMe(), 2000);
             })
         }
     }
     notiSet()
-    notifyMe() 
     // notifyMe()
     // window.localStorage.removeItem('my-list')
     window.createTodoApp = createTodoApp
