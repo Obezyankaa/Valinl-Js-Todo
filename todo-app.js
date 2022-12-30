83664ff96551d07ebdeec703d7f2565c36ed7fbf
VladAir <vladair@MacBook-Air-VladAir.local>
2022-12-30 17:31:38 +0300

    fix 1.6

--- todo-app.js
+++ todo-app.js
@@ -217,32 +217,37 @@
         }) 
     }
 
-    function notifyMe() {
-        var notification = new Notification('Всё сдедал сегодня ?', {
-            tag: 'ache-mail',
-            body: 'загляни в меня',
-            icon: '/img/icons.png'
-        });
-    }
+function notifyMe () {
+		var notification = new Notification ("Все еще работаешь?", {
+			tag : "ache-mail",
+			body : "Пора сделать паузу и отдохнуть",
+			icon : '/img/icons.png'
+		});
+}
+    
+    let testBtn = document.getElementById('testBTN');
+    testBtn.addEventListener('click', function (e) { 
+        e.preventDefault()
+       notifSet()
+    });
 
-    function notiSet() {
-        if (!("Notification" in window))
-            alert('Ваш браузер не поддерживает уведомления.');
-        else if (Notification.permission === 'granted')
-            setTimeout(notifyMe(), 2000);
-        else if (Notification.permission !== "denied") {
-            Notification.requestPermission(function (permission) {
-                if (!('permission' in Notification))
-                    Notification.permission == permission;
-                if (permission === 'granted')
-                    setTimeout(notifyMe(), 2000);
-            })
-        }
-    }
-    notiSet()
-    // notifyMe()
+	
+	function notifSet () {
+		if (!("Notification" in window))
+			alert ("Ваш браузер не поддерживает уведомления.");
+		else if (Notification.permission === "granted")
+			setTimeout(notifyMe, 2000);
+		else if (Notification.permission !== "denied") {
+			Notification.requestPermission (function (permission) {
+				if (!('permission' in Notification))
+					Notification.permission = permission;
+				if (permission === "granted")
+					setTimeout(notifyMe, 2000);
+			});
+		}
+	}
     // window.localStorage.removeItem('my-list')
     window.createTodoApp = createTodoApp
 })();
 
-console.log('version: 1.5');
+console.log('version: 1.6');
