8d76c0d807405367ea256af2385a6973402761e7
VladAir <vladair@MacBook-Air-VladAir.local>
2022-12-30 16:49:08 +0300

    fix setTimer

--- todo-app.js
+++ todo-app.js
@@ -216,6 +216,31 @@
             todoItemForm.input.value = ''            
         }) 
     }
+
+    function notifyMe() {
+        var notification = new Notification('Всё сдедал сегодня ?', {
+            tag: 'ache-mail',
+            body: 'загляни в меня',
+            icon: '/img/icons.png'
+        });
+    }
+
+    function notiset() {
+        if (!("Notification" in window))
+            alert('Ваш браузер не поддерживает уведомления.');
+        else if (Notification.permission === 'granted')
+            setTimeout(notifyMe, 2000);
+        else if (Notification.permission !== "default") {
+            Notification.requestPermission(function (permission) {
+                if (!('permission' in Notification))
+                    Notification.permission == permission;
+                if (permission === 'granted')
+                    setTimeout(notifyMe, 2000);
+            })
+        }
+    }
+    notiset()
+    notifyMe()
     // window.localStorage.removeItem('my-list')
     window.createTodoApp = createTodoApp
 })();
